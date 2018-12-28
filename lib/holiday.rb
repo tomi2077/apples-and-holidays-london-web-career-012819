3df9580d8a7145f676d3bf2598b23e5cd6a68168
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-28 02:43:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  return holiday_hash.flatten.values
+  return holiday_hash[:winter].flatten.values
 
 end
 
