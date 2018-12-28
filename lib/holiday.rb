e032cb978b04f792f5d5cf805e4bdeb13b7bc526
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-28 02:24:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash.each do |season, holiday_name|
-    season << holiday_name << supply_array
+    season.push(holiday_name).push(supply_array)
   end
   return holiday_hash
 end
