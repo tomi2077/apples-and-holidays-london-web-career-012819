fed026f156d5036e216f777aab1a2b7712775aeb
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-28 02:32:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,7 +37,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday.each do |season|
+  holiday_hash.each do |season|
     season << holiday_name
     holiday_name.push(supply_array)
   end
