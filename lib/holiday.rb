81b2cd027a6e8a964f521c73fd0f62bf824c02d9
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-28 03:29:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season,holiday|
-    holiday.each do |holiday, activity|
+    holiday.each do |holiday, activities|
+      activities.each do |activity|
       if activity == "BBQ"
         
 
